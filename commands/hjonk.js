35dc4c68c231bcaad5a47cf5f463fdf98afd640a
datejer <ejer.priv@wp.pl>
2021-04-12 09:46:18 +0200

    Add option to set amount of hjonks per voice channel join

--- commands/hjonk.js
+++ commands/hjonk.js
@@ -6,6 +6,9 @@ exports.run = (client, message, args) => {
 	let voiceChannel = message.member.voice.channel;
 	if (!voiceChannel) return message.reply("voic chanel pls");
 
+	let repeat = 1;
+	if (args[0] && args[0] > 1 && args[0] < 30) repeat = args[0];
+
 	if (client.playing.has(message.guild.id)) return;
 	client.playing.add(message.guild.id);
 
@@ -16,14 +19,32 @@ exports.run = (client, message, args) => {
 				if (err) return console.error(err);
 				let sound = files[Math.floor(Math.random() * files.length)];
 
-				const dispatcher = connection.play(directory + sound);
 				message.channel.send("hjönk");
-				dispatcher.on("speaking", (speaking) => {
-					if (speaking === 0) {
-						voiceChannel.leave();
-						return client.playing.delete(message.guild.id);
-					}
-				});
+				if (repeat > 1) {
+					const honkOnRepeat = () => {
+						const dispatcher = connection.play(directory + sound);
+						dispatcher.on("speaking", (speaking) => {
+							if (speaking === 0) {
+								if (repeat <= 1) {
+									voiceChannel.leave();
+									return client.playing.delete(message.guild.id);
+								} else {
+									repeat--;
+									return honkOnRepeat();
+								}
+							}
+						});
+					};
+					honkOnRepeat();
+				} else {
+					const dispatcher = connection.play(directory + sound);
+					dispatcher.on("speaking", (speaking) => {
+						if (speaking === 0) {
+							voiceChannel.leave();
+							return client.playing.delete(message.guild.id);
+						}
+					});
+				}
 			});
 		})
 		.catch((err) => console.log(err));
@@ -31,6 +52,7 @@ exports.run = (client, message, args) => {
 
 exports.help = {
 	name: "hjonk",
-	description: "Play the honk sound.",
-	usage: "hjonk",
+	description:
+		"Play the honk sound. You can set the amount of hjonks, or leave the argument blank to play it just once. The maximum amount is 30.",
+	usage: "hjonk [amount]",
 };
